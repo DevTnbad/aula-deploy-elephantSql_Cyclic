cde758649c7cabc9de2dacec7a4fb454ea7586c1
DevTnbad <dev.tnbad@gmail.com>
2023-10-17 09:48:43 -0300

    ssl redefinido no conexao.js

--- src/conexao.js
+++ src/conexao.js
@@ -6,8 +6,8 @@ const knex = require('knex')({
       user : process.env.DB_USER,
       password : process.env.DB_PASS,
       database : process.env.DB_DATABASE,
-      ssl: config["DB_SSL"] ? { rejectUnauthorized: false } : false,
-      // ssl: { rejectUnauthorized: false }
+      // ssl: config["DB_SSL"] ? { rejectUnauthorized: false } : false,
+      ssl: { rejectUnauthorized: false }
     }
   });
 
