0f784b44da38f43fbd96ee7db8fd209a6cb08e90
DevTnbad <dev.tnbad@gmail.com>
2023-10-17 08:31:24 -0300

    configurado conexao.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
 const express = require('express');
+const knex = require('knex');
 const app = express();
 
 app.use(express.json());
 
+app.get('/', async (req, res)=> {
+    try {
+        const carros = await knex('carros');
+        return res.status(200).json(carros);
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Erro interno do servidor')
+    }
+});
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, console.log(`Servidor Up na porta: ${port}`));
+app.listen(port, () => console.log(`Servidor Up na porta: ${port}`));
